2268b9b113428b5ba2ecd3f19cb2121560b6af1c
vitalii-kyiv <v.s.chernous@gmail.com>
2024-12-04 15:55:24 +0200

    tasks_1-4

---
+++ 1/1.py
@@ -0,0 +1,21 @@
+def total_salary(path: str) -> tuple:
+    try:
+        with open(path, 'r', encoding="utf-8") as file:
+            salary_data = file.readlines()
+        total = 0
+
+        for person_salary in salary_data:
+            salary = float(person_salary.strip("").split(",")[1])
+            total += salary
+
+        return (total, total/len(salary_data))
+    
+    except FileNotFoundError:
+        print("Файл не знайдено. Перевірте шлях до файлу.")
+        return None
+    except ValueError:
+        print("Помилка обробки даних у файлі. Перевірте формат.")
+        return None
+
+
+path_to_file ="1/salary.txt"
